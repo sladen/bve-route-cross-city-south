edb89242f1cdde56cb8bced53d0143c8af0e6fdf
Paul Sladen <paul@sladen.org>
2009-02-26 07:17:48 +0200

    Fix symlink-duplicates.sh to work correctly on current directory Recode .wav files to .ogg to save space

--- debian/symlink-duplicates.sh
+++ debian/symlink-duplicates.sh
@@ -7,7 +7,9 @@ for dir in "$@" ; do
 	find . -type f -print0 | xargs -0 md5sum | sort \
 	| while read -r new_checksum new_filename ; do
 	    if [ "$new_checksum" = "$checksum" -a -f "$new_filename" -a -f "$filename" ] ; then
-		symlink_path="`dirname \"$new_filename\" | sed -e 's|/[^./][^/]*|/..|g'|cut -c3-`/`echo \"$filename\"|cut -c3-`"
+		# This takes full/path/names/to/file.foo and replaces each all of the levels
+		# with '..' (so './../../../../').  Then appends destination $file name.
+		symlink_path="`dirname \"$new_filename\" | sed -e 'sX/[^\./][^/]*X/..Xg'`/`echo \"$filename\"|cut -c3-`"
 		rm "$new_filename" && ln -s "$symlink_path" "$new_filename" 
 	    else
 		checksum="$new_checksum"
